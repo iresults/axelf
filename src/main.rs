4fbedf0be0b486e50a876826b66204bce17254a0
Daniel Corn <daniel@iresults.li>
2021-09-30 11:50:02 +0200

    Add `--check` flag to check process without launching

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use crate::error::{Error, TriState};
 use crate::launcher::Launcher;
 use crate::pid_file::{Pid, PidFile};
 use std::env;
+use std::env::Args;
 
 extern crate log;
 
@@ -21,46 +22,106 @@ fn main() {
 }
 
 fn run() -> Result<(), Error> {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 3 {
-        let message = format!(
-            r"Axel F will take care that executable is running
+    let arguments = get_arguments(env::args())?;
+    let mut pid_file = PidFile::open(&arguments.pid_file_path)?;
+    let (message, must_start_process) = check_process(&arguments.executable_path, &mut pid_file)?;
 
-Usage {} /path/to/executable /path/to/pid-file",
-            &args[0]
-        );
-
-        return Err(error::Error::Usage(message));
-    }
-
-    let executable_path = &args[1];
-    let pid_file_path = &args[2];
-
-    let mut pid_file = PidFile::open(pid_file_path)?;
-
-    let must_start_process = match pid_file.read_pid() {
-        TriState::Some(pid) => {
-            if check_if_process_is_running(pid, executable_path)? {
-                log::info!("Process with PID {} is running -> don't start process", pid);
-                false
-            } else {
-                log::info!("Process with PID {} not running -> must start process", pid);
-                true
+    match arguments.command.as_deref() {
+        Some("check") => {
+            println!("{}", message);
+        }
+        Some("help") => {
+            println!("{}", get_help(&arguments));
+        }
+        _ => {
+            log::info!("{}", message);
+            if must_start_process {
+                let launcher = Launcher::new(pid_file, &arguments.executable_path);
+                let pid = launcher.launch()?;
+                log::info!("Launched process with PID {}", pid);
             }
         }
-        TriState::None => {
-            log::info!("No PID stored -> must start process");
-            true
-        }
-        TriState::Error(e) => return Err(e),
-    };
-
-    if must_start_process {
-        let launcher = Launcher::new(pid_file, executable_path);
-        let pid = launcher.launch()?;
-        log::info!("Launched process with PID {}", pid);
     }
     Ok(())
+}
+
+fn get_help(args: &Arguments) -> String {
+    format!(
+        r"Axel F will take care that executable is running
+
+Usage {} /path/to/executable /path/to/pid-file",
+        args.self_path
+    )
+}
+
+fn check_process(executable_path: &str, pid_file: &mut PidFile) -> Result<(String, bool), Error> {
+    match pid_file.read_pid() {
+        TriState::Some(pid) => {
+            if check_if_process_is_running(pid, executable_path)? {
+                Ok((
+                    format!("Process with PID {} is running -> don't start process", pid),
+                    false,
+                ))
+            } else {
+                Ok((
+                    format!("Process with PID {} not running -> must start process", pid),
+                    true,
+                ))
+            }
+        }
+        TriState::None => Ok((format!("No PID stored -> must start process"), true)),
+        TriState::Error(e) => Err(e),
+    }
+}
+
+struct Arguments {
+    self_path: String,
+    executable_path: String,
+    pid_file_path: String,
+    command: Option<String>,
+}
+
+fn get_arguments(args: Args) -> Result<Arguments, Error> {
+    let mut self_path = None;
+    let mut executable_path = None;
+    let mut pid_file_path = None;
+    let mut command = None;
+
+    for arg in args {
+        if self_path.is_none() {
+            // The very first argument is the executable path
+            self_path = Some(arg.to_owned());
+        } else if arg.starts_with("--") {
+            command = arg.strip_prefix("--").map(str::to_owned)
+        } else if executable_path.is_none() {
+            // The first argument without "--" is the executable path
+            executable_path = Some(arg.to_owned());
+        } else if pid_file_path.is_none() {
+            // The second argument without "--" is the PID file path
+            pid_file_path = Some(arg.to_owned());
+        } else {
+            return Err(Error::Usage("To many arguments".to_owned()));
+        }
+    }
+
+    if self_path.is_none() {
+        return Err(Error::Usage("Missing self path".to_owned()));
+    }
+    if executable_path.is_none() {
+        return Err(Error::Usage(
+            "Missing argument 'executable_path'".to_owned(),
+        ));
+    }
+    if pid_file_path.is_none() {
+        return Err(Error::Usage("Missing argument 'pid_file'".to_owned()));
+    }
+
+    Ok(Arguments {
+        self_path: self_path.unwrap(),
+        executable_path: executable_path.unwrap(),
+        pid_file_path: pid_file_path.unwrap(),
+        command,
+    })
 }
 
 fn check_if_process_is_running(pid: Pid, executable_path: &str) -> crate::error::Result<bool> {
